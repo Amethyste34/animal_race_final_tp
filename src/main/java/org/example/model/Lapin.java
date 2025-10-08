973067b1a29c2f538acb5b4a8b215fd394d26050
Amethyste34 <laurence.amethyste@gmail.com>
2025-10-08 12:47:03 +0200

    Implémentation de la classe Lapin

--- src/main/java/org/example/model/Lapin.java
+++ src/main/java/org/example/model/Lapin.java
@@ -1,5 +1,51 @@
 package org.example.model;
 
+/**
+ * Classe qui représente un lapin : rapide mais irrégulier
+ * Implémente la sealed interface Animal
+ */
 public final class Lapin implements Animal {
 
+    /**
+     * Attribut Nom de l'animal
+     */
+    private final String nom;
+
+    /**
+     * Constructeur
+     * @param nom
+     */
+    public Lapin(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String getNom() {
+        return nom;
+    }
+
+    @Override
+    public String getEmoji() {
+        return "\uD83D\uDC07";
+    }
+
+    @Override
+    public int getVitesseMin() {
+        return 100;
+    }
+
+    @Override
+    public int getVitesseMax() {
+        return 600;  // Rapide
+    }
+
+    @Override
+    public int getPasMin() {
+        return 1;
+    }
+
+    @Override
+    public int getPasMax() {
+        return 4;  // Peut faire de grands bonds
+    }
 }
